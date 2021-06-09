56c0aefe8ab50b8debf5e81103a60457813de60c
Franklin Graham <grahamfranklin329@gmail.com>
2021-06-09 10:22:09 -0700

    Update settings.py

--- portfolio-Web/portfolio_project/settings.py
+++ portfolio-Web/portfolio_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,6 +143,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
 
 #mongodb+srv://franklin:franklin@329@franklin.j8xgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
