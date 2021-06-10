d36b65b1a4ec68794aba827aa2295d422d3c7093
Franklin Graham <grahamfranklin329@gmail.com>
2021-06-09 22:44:15 -0700

    Update settings.py

--- portfolio-Web/portfolio_project/settings.py
+++ portfolio-Web/portfolio_project/settings.py
@@ -166,7 +166,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
