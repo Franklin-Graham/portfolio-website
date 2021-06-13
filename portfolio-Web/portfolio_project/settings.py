fdd84d70dc9dbeb40b848d4598397d29aa01cbe5
Franklin Graham <grahamfranklin329@gmail.com>
2021-06-13 18:36:59 +0530

    Update settings.py

--- portfolio-Web/portfolio_project/settings.py
+++ portfolio-Web/portfolio_project/settings.py
@@ -174,12 +174,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 if DEBUG:
-
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 else:
-
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
   
 
