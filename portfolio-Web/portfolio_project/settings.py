61d0d83f067a30f0e180ab56b20ac3ea2738862e
Franklin Graham <grahamfranklin329@gmail.com>
2021-06-13 18:29:35 +0530

    Update settings.py

--- portfolio-Web/portfolio_project/settings.py
+++ portfolio-Web/portfolio_project/settings.py
@@ -169,10 +169,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+#STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+if DEBUG:
+
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+  
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
